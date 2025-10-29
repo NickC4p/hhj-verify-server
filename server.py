fdfadc33a557689be785e3595c1fd4df5b9c6428
Nick.c <229480797+NickC4p@users.noreply.github.com>
2025-10-29 08:38:13 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ import smtplib, random, re
 app = Flask(__name__)
 CORS(app)
 
+if __name__ == "__main__":
+port  = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
 # memorizziamo temporaneamente il codice in memoria
 stored_code = None
 
